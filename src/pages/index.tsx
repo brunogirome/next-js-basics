89a6d26a39ed22b082b448931e516cd672bf711f
Bruno Girome <brunogirome@gmail.com>
2020-10-10 16:24:51 -0300

    Introduction to client side fetching
    Used when you dont need to search engines read the data, since it use javascript on client side.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,38 @@
-import { Title } from '../styles/pages/Home';;
+import { useEffect, useState } from 'react';
+import { Title } from '../styles/pages/Home';
+
+interface IProduct {
+  id: number;
+  title: string;
+}
 
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data);
+      })
+    });
+
+  }, [])
+
   return (
-    <Title>
-      <h1>Hello World :)</h1>
-    </Title>
+    <div>
+      <section>
+        <Title>Producuts</Title>
+
+        <ul>
+          {recommendedProducts.map(recommendedProduct => {
+            return (
+              <li key={recommendedProduct.id}>
+                {recommendedProduct.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </div>
   )
 }
