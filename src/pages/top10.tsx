9f680939701ac684ee9741d4f592e247bc788e39
Bruno Girome <brunogirome@gmail.com>
2020-10-11 16:47:23 -0300

    Introduction to static site generation
    Generate the html of the page on the server, used on pages that dont uses database data. Generally used on blog posts, since it is mostly never change the information

---
+++ src/pages/top10.tsx
@@ -0,0 +1,39 @@
+import { GetStaticProps } from "next";
+
+interface IProduct {
+  id: number;
+  title: string;
+}
+
+interface ITop10Props {
+  products: IProduct[];
+}
+
+export default function Top10({ products }: ITop10Props) {
+  return (
+    <div>
+      <h1>Top 10 products</h1>
+
+      <ul>
+          {products.map((product) => {
+            return (
+              <li key={product.id}>{product.title}</li>
+            );
+          })}
+        </ul>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps<ITop10Props> = async (context) => {
+  const response = await fetch("http://localhost:3333/products");
+
+  const products = await response.json();
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 10,
+  }
+}
